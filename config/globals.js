5416435413ebb862444226a09f4a641851c89772
Tom Elliott <tommygunsster@gmail.com>
2020-05-12 10:31:17 -0500

    Make plugin inject menu item where we want

--- config/globals.js
+++ config/globals.js
@@ -2,36 +2,31 @@ const array_insert_before = function(text, item, insert) {
   let index = undefined;
   for (var _i = 0, _len = item.length;_i < _len;_i++) {
     if (item[_i].text === text) {
-      index = _i - 1;
+      index = _i;
       break;
     }
   }
-  if (undefined === index) {
-    item.splice(0, 0, insert);
-  }
-  sails.log.info(require('util').inspect(item, {depth: null}));
+  item.splice(index, 0, insert);
   return item;
-}
-module.exports = function() {
-  let menuItems = {
-    text: 'Snapin',
-    plural: 'Snapins',
-    link: '/snapins',
-    icon: 'fa-cube',
-    subLinks: [
-      {
-        link: '/snapins/create',
-        text: 'Create New Snapin'
-      },
-      {
-        link: '/snapins/export',
-        text: 'Export Snapins'
-      },
-      {
-        link: '/snapins/import',
-        text: 'Import Snapins',
-      }
-    ]
-  };
-  sails.config.globals.menuItems = array_insert_before('User', sails.config.globals.menuItems, menuItems);
 };
+let menuItems = {
+  text: 'Snapin',
+  plural: 'Snapins',
+  link: '/snapins',
+  icon: 'fa-cube',
+  subLinks: [
+    {
+      link: '/snapins/create',
+      text: 'Create New Snapin'
+    },
+    {
+      link: '/snapins/export',
+      text: 'Export Snapins'
+    },
+    {
+      link: '/snapins/import',
+      text: 'Import Snapins',
+    }
+  ]
+};
+sails.config.globals.menuItems = array_insert_before('User', sails.config.globals.menuItems, menuItems);
